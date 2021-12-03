f4117c0d7dfd035d9c68f8ff253a6679c5ed6164
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2021-12-03 19:11:19 +0100

    Add tests for hand to element mapping

--- tests/CommandLineInterface_test.cpp
+++ tests/CommandLineInterface_test.cpp
@@ -17,7 +17,7 @@ class CommandLineInterfaceTest : public ::testing::Test {
   }
 };
 
-TEST_F(CommandLineInterfaceTest, getNumberOfRounds_positiveNumber) {
+TEST_F(CommandLineInterfaceTest, getNumberOfRounds__positive_number) {
   // Given
   std::string user_input = "22";
   CommandLineInterface cli = create_SUT_with(user_input);
@@ -26,11 +26,38 @@ TEST_F(CommandLineInterfaceTest, getNumberOfRounds_positiveNumber) {
   EXPECT_EQ(cli.getNumberOfRounds(), 22);
 }
 
-TEST_F(CommandLineInterfaceTest, getNumberOfRounds_negativeNumber) {
+TEST_F(CommandLineInterfaceTest, getNumberOfRounds__negative_number) {
   // Given
   std::string user_input = "-47";
   CommandLineInterface cli = create_SUT_with(user_input);
 
   // When
   EXPECT_EQ(cli.getNumberOfRounds(), -47);
+}
+
+TEST_F(CommandLineInterfaceTest, getPlayerAction__map_closed_fist_to_rock) {
+  // Given
+  std::string user_input = "F";
+  CommandLineInterface cli = create_SUT_with(user_input);
+
+  // When
+  EXPECT_EQ(cli.getPlayerAction(), Element::Rock);
+}
+
+TEST_F(CommandLineInterfaceTest, getPlayerAction__map_open_hand_to_paper) {
+  // Given
+  std::string user_input = "H";
+  CommandLineInterface cli = create_SUT_with(user_input);
+
+  // When
+  EXPECT_EQ(cli.getPlayerAction(), Element::Paper);
+}
+
+TEST_F(CommandLineInterfaceTest, getPlayerAction__map_two_fingers_to_scissors) {
+  // Given
+  std::string user_input = "V";
+  CommandLineInterface cli = create_SUT_with(user_input);
+
+  // When
+  EXPECT_EQ(cli.getPlayerAction(), Element::Scissors);
 }
