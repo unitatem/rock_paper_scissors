be80173a3305921b365b81a25202271acf3500b5
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2021-12-05 01:45:35 +0100

    Fix googletest version

--- tests_libs/googletest/googletest-download.cmake
+++ tests_libs/googletest/googletest-download.cmake
@@ -7,7 +7,7 @@ project(googletest-download NONE)
 include(ExternalProject)
 ExternalProject_Add(googletest
         GIT_REPOSITORY https://github.com/google/googletest.git
-        GIT_TAG main
+        GIT_TAG release-1.11.0
         SOURCE_DIR "@DESTINATION_ROOT@/googletest-src"
         BINARY_DIR "@DESTINATION_ROOT@/googletest-build"
         CONFIGURE_COMMAND ""
